d65748ef67a19e194eeb34dea821d379de27f47e
Mayara <mayarafvieira@gmail.com>
2023-01-25 20:34:33 -0300

    feat(BancoView): adiciona o menu de Login e alguns métodos de validação para ele. Ainda não tá completo.

--- src/br/com/ada/views/banco/BancoView.java
+++ src/br/com/ada/views/banco/BancoView.java
@@ -4,7 +4,11 @@ import br.com.ada.banco.Banco;
 import br.com.ada.clientes.Cliente;
 import br.com.ada.clientes.ClienteFisico;
 import br.com.ada.clientes.ClienteJuridico;
+import br.com.ada.repositorio.cliente.RepositorioClienteFisico;
+import br.com.ada.repositorio.cliente.RepositorioClienteJuridico;
 import br.com.ada.views.View;
+
+import java.util.List;
 
 //todo - será que tavez não seja melhor criar tipo uma 'viewCadastro'
 public class BancoView extends View {
@@ -28,6 +32,7 @@ public class BancoView extends View {
                     break;
                 case 2:
                     //todo - criar função nessa classe para login
+                    menuLogin();
                     // depois do login deverá chamar um menu do cliente
                     System.out.println("Foi escolhida a opção acessar conta");
                     break;
@@ -84,4 +89,91 @@ public class BancoView extends View {
         return cnpj;
         //todo - validar cnpj
     }
+
+    private void menuLogin() {
+        System.out.println("Selecione se você é pessoa jurídica ou física:");
+        System.out.println("Digite 1 para pessoa jurídica.");
+        System.out.println("Digite 2 para pessoa física.");
+        System.out.println("Digite 3 para retornar ao menu inicial");
+        int tipoPessoa = Integer.parseInt(sc.nextLine());
+
+        switch (tipoPessoa){
+            case 1:
+                menuLoginClienteJuridico();
+                break;
+            case 2:
+                menuLoginClienteFisico();
+                break;
+            case 3:
+                menuInicial();
+            default:
+                System.out.println("Opção inválida, por favor digite novamente");
+                menuCadastro();
+        }
+
+    }
+
+    //todo - onde colocar esses dois métodos?
+    private boolean isCnpjCadastrado(String cnpj) {
+        boolean isCnpjCadastrado = false;
+        List<ClienteJuridico> clientes = RepositorioClienteJuridico.getInstance().retornarClientesJuridico();
+        for (ClienteJuridico cliente : clientes) {
+            if (cliente.getCnpj() == cnpj) {
+                isCnpjCadastrado = true;
+            }
+        }
+        return isCnpjCadastrado;
+    }
+    private boolean isCpfCadastrado(String cpf) {
+        boolean isCpfCadastrado = false;
+        List<ClienteFisico> clientes = RepositorioClienteFisico.getInstance().retornarClientesFisico();
+        for (ClienteFisico cliente : clientes) {
+            if (cliente.getCpf() == cpf) {
+                isCpfCadastrado = true;
+            }
+        }
+        return isCpfCadastrado;
+    }
+
+    private void menuLoginClienteJuridico() {
+        String cnpj = pedirCnpj();
+        if (!isCnpjCadastrado(cnpj)) {
+            //exceção cnpj não cadastrado
+            System.out.println("esse cnpj não esta cadastrado");
+            return;
+        }
+
+        ClienteJuridico cliente = RepositorioClienteJuridico.getInstance().retornarClientePorCnpj(cnpj);
+
+        System.out.println("Digite sua senha:");
+        String senhaInserida = sc.nextLine();
+        if (!cliente.verificaSenha(senhaInserida)) {
+            //todo - exceção para senha errada
+            System.out.println("senha inválida");
+        }
+        System.out.println("cliente juridico logado");
+        //menuInicialClienteJuridico(Cliente)
+    }
+
+    private void menuLoginClienteFisico() {
+        String cpf = pedirCpf();
+        if (!isCpfCadastrado(cpf)) {
+            //exceção cpf não cadastrado
+            System.out.println("esse cpf não esta cadastrado");
+            return;
+        }
+
+        ClienteFisico cliente = RepositorioClienteFisico.getInstance().retornarClientePorCpf(cpf);
+
+        System.out.println("Digite sua senha:");
+        String senhaInserida = sc.nextLine();
+        if (!cliente.verificaSenha(senhaInserida)) {
+            //todo - exceção para senha errada
+            System.out.println("senha inválida");
+        }
+        System.out.println("cliente fisico logado");
+        //menuInicialClienteFisico(Cliente)
+    }
+
+
 }
